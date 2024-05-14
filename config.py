c0af3330b46292fc0541102edc68fa9f725dd197
Abel Gladstone Mangam <agladstonemangam@mmm.com>
2024-05-14 11:30:52 +0200

    Add global config set

--- config.py
+++ config.py
@@ -1,83 +1,103 @@
 # Description: A module to store the config data.
 # because python_dotenv does not consider the environment variables set in the .env file. which are already set in the system.
 # This module is used to store the config data in a class and access it using the class attributes.
-import typing as ty
+import warnings
 import tomllib
 from singleton import SingleTon
 
 
-class ProjConfigNameSpace:
-    """A class to store the config data.
-    With config data as attributes of the class.
-    """
+class ProjConfigChangedWarning(UserWarning):
+    """A warning to indicate that the config has been changed."""
 
-    def __init__(self, **kwargs):
+
+class ProjConfigNameSpace:
+    """A class to store the config data."""
+
+    def __init__(self, config_name, **kwargs):
+        self._name_ = config_name
         self.__dict__.update(kwargs)
+        for k, v in kwargs.items():
+            if isinstance(v, dict):
+                setattr(self, k, ProjConfigNameSpace(k, **v))
+
+    def get_name(self) -> str:
+        """Get the name of the config."""
+        return self._name_
+
+    def __str__(self):
+        output = f"\n{self._name_}:\n"
+        for k, v in self.__dict__.items():
+            if k.startswith("_"):
+                continue
+            if isinstance(v, ProjConfigNameSpace):
+                output += f"    {v.__str__()}\n"
+            else:
+                output += f"\t{v}\n"
+        return output
 
 
 class ProjConfig(SingleTon):
     """A class to store the config data."""
 
-    config_file_name = ""
-
-    def __init__(self):
-        self._data = {}
+    _config_file = None
+    _current_config = None
+    _available_configs = []
+    data = {}
 
     @property
-    def data(self):
-        """Get the config data."""
-        return self._data
+    def config(self) -> ProjConfigNameSpace:
+        """Get the current config."""
+        return self._current_config
 
-    @data.setter
-    def data(self, data: ty.Dict[str, ty.Any]):
-        self._data = data
-        for key, value in data.items():
-            setattr(self, key, value)
+    @config.setter
+    def config(self, config_name: str):
+        """Set the current config."""
+        if config_name not in self._available_configs:
+            raise AttributeError(f"Config {config_name} not found.")
+        if self.config:
+            previous_warning_format = warnings.formatwarning
+            ## custom warning message format to include the class name and the message
+            warnings.formatwarning = lambda m, c, *_: f"\n{c.__name__}: {m}\n"
+            warnings.warn(f"Config changed from {self.config.get_name()} to {config_name}.", ProjConfigChangedWarning)
+            warnings.formatwarning = previous_warning_format
+        self._current_config = ProjConfigNameSpace(config_name, **self.data[config_name])
+
+    @property
+    def config_file(self):
+        """Get the config file name."""
+        return self._config_file
+
+    @config_file.setter
+    def config_file(self, file_name: str):
+        """Set the config file name."""
+        self._config_file = file_name
+        self._reload_config()
+
+    def _reload_config(self):
+        """Reload the config data."""
+        with open(self._config_file, "rb") as file:
+            self.data = tomllib.load(file)
+            self._available_configs = list(self.data.keys())
+            self._current_config = None
 
     @classmethod
-    def load(cls, file_path: str):
+    def load_from_toml(cls, file_path: str) -> "ProjConfig":
         """Load the config data from a toml file."""
-        with open(file_path, "rb") as f:
-            ProjConfig.config_file_name = file_path
-            data = tomllib.load(f)
-            config = cls()
-            config.data = data
-            return config
+        obj = cls()
+        obj.config_file = file_path
+        return obj
 
 
-def load(file_path: str):
-    """Load the config data."""
-    ProjConfig.load(file_path)
-
-
-def get(config_name: str, key: str) -> ty.Any:
-    """Get the config value."""
-    try:
-        config = get_config(config_name)
-        return getattr(config, key)
-    except AttributeError as e:
-        raise AttributeError(f"Config {config_name} not found.") from e
-
-
-def get_config(config_name: str) -> ProjConfigNameSpace:
-    """Get the config data."""
-    try:
-        config = getattr(ProjConfig(), config_name)
-        return ProjConfigNameSpace(**config)
-    except AttributeError as e:
-        raise AttributeError(f"Config {config_name} not found.") from e
-
-
-def get_config_file_name():
-    """Get the config file name."""
-    return ProjConfig.config_file_name
+def get_config() -> ProjConfigNameSpace:
+    """Get the config data. A helper function to get the config data. that can be used in other modules."""
+    return ProjConfig().config
 
 
 # Example usage
 if __name__ == "__main__":
     FILE_NAME = "example_config.toml"
-    load(FILE_NAME)
-    prod_config = get_config("production")
-    dev_config = get_config("development")
-    print(prod_config)
-    print(dev_config)
+    cfg = ProjConfig.load_from_toml(FILE_NAME)
+    cfg.config = "development"
+    print(cfg.config)
+    cfg.config = "production"
+    print(cfg.config)
--- test/test_proj_config.py
+++ test/test_proj_config.py
@@ -1,3 +1,7 @@
+## Add MIT License
+# -*- coding: utf-8 -*-
+# File: test_proj_config.py
+"""Test the ProjConfig class."""
 import unittest
 from unittest.mock import patch
 import config as pcfg
@@ -10,40 +14,65 @@ key2 = "value2"
 key1 = "value1"
 key2 = "value2"
 num = 1
+[cfg3]
+row1.key1 = "11"
+row1.key2 = "12"
+row2.key1 = "21"
+row2.key2 = "22"
 """
+
+
+class ProjConfigCreateTests(unittest.TestCase):
+
+    def test_create(self):
+        cfg = pcfg.ProjConfig()
+        self.assertIsNone(cfg.config_file)
+        self.assertIsNone(cfg.config)
+        self.assertEqual(cfg.data, {})
 
 
 class ProjConfigTests(unittest.TestCase):
 
     @patch("builtins.open")
-    def test_load(self, mock_open):
-        # mock open to return a file object with the test toml data as bytes
+    def setUp(self, mock_open) -> None:
+        """Set up the test case by loading the config. from the TEST_TOML string."""
         mock_open.return_value.__enter__.return_value.read.return_value = bytes(TEST_TOML, "utf-8")
+        pcfg.ProjConfig.load_from_toml("test.toml")
+
+    def test_load(self):
+        """Test the config loading."""
+        cfg = pcfg.ProjConfig()
+        self.assertEqual(cfg.config_file, "test.toml")
         # load the config
-        pcfg.load("test.toml")
-        cfg1_data = pcfg.get_config("cfg1")
-        cfg2_data = pcfg.get_config("cfg2")
+        cfg.config = "cfg1"
+        cfg1_data = cfg.config
         # check the config data
+        self.assertEqual(cfg1_data.get_name(), "cfg1")
         self.assertEqual(cfg1_data.key1, "value1")
         self.assertEqual(cfg1_data.key2, "value2")
+        # check the second config
+        with self.assertWarns(pcfg.ProjConfigChangedWarning) as cm:
+            cfg.config = "cfg2"
+        cfg2_data = cfg.config
+        # check the second config data
+        self.assertEqual(cfg.config.get_name(), "cfg2")
         self.assertEqual(cfg2_data.key1, "value1")
         self.assertEqual(cfg2_data.key2, "value2")
         self.assertEqual(cfg2_data.num, 1)
+        # check the third config
+        with self.assertWarns(pcfg.ProjConfigChangedWarning) as cm:
+            cfg.config = "cfg3"
+        cfg3_data = cfg.config
+        # check the third config data
+        self.assertEqual(cfg.config.get_name(), "cfg3")
+        self.assertEqual(cfg3_data.row1.key1, "11")
+        self.assertEqual(cfg3_data.row1.key2, "12")
+        self.assertEqual(cfg3_data.row2.key1, "21")
+        self.assertEqual(cfg3_data.row2.key2, "22")
 
-    @patch("builtins.open")
-    def test_get(self, mock_open):
-        mock_open.return_value.__enter__.return_value.read.return_value = bytes(TEST_TOML, "utf-8")
-        pcfg.load("test.toml")
-        # check the config data
-        self.assertEqual(pcfg.get("cfg1", "key1"), "value1")
-        self.assertEqual(pcfg.get("cfg1", "key2"), "value2")
-        self.assertEqual(pcfg.get("cfg2", "key1"), "value1")
-        self.assertEqual(pcfg.get("cfg2", "key2"), "value2")
-        self.assertEqual(pcfg.get("cfg2", "num"), 1)
-        self.assertRaises(AttributeError, pcfg.get, "cfg3", "key1")
-
-    @patch("builtins.open")
-    def test_get_config_file_name(self, mock_open):
-        mock_open.return_value.__enter__.return_value.read.return_value = bytes(TEST_TOML, "utf-8")
-        pcfg.load("test.toml")
-        self.assertEqual(pcfg.get_config_file_name(), "test.toml")
+    def test_get_config(self):
+        cfg = pcfg.ProjConfig()
+        self.assertIsNone(cfg.config)
+        cfg.config = "cfg1"
+        self.assertIsNotNone(cfg.config)
+        self.assertEqual(pcfg.get_config(), cfg.config)
